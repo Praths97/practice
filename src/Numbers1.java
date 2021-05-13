a1e38a34699483b6ac4785b42d6371711c63151b
Praths97 <prathibhareddy78@gmail.com>
2021-05-13 23:32:33 +0530

    Iterator example

---
+++ src/Numbers1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Numbers1 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Iterator it=  numbers.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
