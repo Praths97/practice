6a1d4df2326afa20a7b9cff625152100270d4771
Praths97 <prathibhareddy78@gmail.com>
2021-05-13 23:47:08 +0530

    classname renamed

--- src/IteratorExample.java
+++ src/IteratorExample.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Numbers1 {
+public class IteratorExample {
     public static void main(String[] args) {
 
         List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
