9eca2be7f20d635fbeaaec798d0d60cad74947d5
Praths97 <prathibhareddy78@gmail.com>
2021-05-23 00:32:45 +0530

    Prathibha Collection Q4

---
+++ src/exam/UpperCase.java
@@ -0,0 +1,15 @@
+package exam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        Map<String, String> upmap = new HashMap<>();
+        String a1[] = new String[]{"goa", "kerala", "gujarat"};
+        for (String s : a1) {
+            upmap.put(s.substring(0, 3).toUpperCase(), s);
+        }
+        System.out.println(upmap);
+    }
+}
