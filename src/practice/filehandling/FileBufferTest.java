4bc6df6ecd7ed82f5117f269c9f7a5f30b3f9232
Praths97 <prathibhareddy78@gmail.com>
2021-05-24 16:36:55 +0530

    24 may task1 - FileInputStream

--- src/practice/filehandling/FileBufferTest.java
+++ src/practice/filehandling/FileBufferTest.java
@@ -1,2 +1,25 @@
-package practice.filehandling;public class FileBufferTest {
+package practice.filehandling;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class FileBufferTest {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("C:/Prathibha/practice/Data.txt");
+            BufferedInputStream bin = new BufferedInputStream(fin);
+            byte[] buf = new byte[100];
+            long total = 0;
+            System.out.print("The content of the file read using is BufferedInputStream :");
+            while (true) {
+                int r = bin.read(buf);
+                if (r == -1) {
+                    break;
+                }
+            } String s = new String(buf);
+            System.out.print( s);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
